259992ce2a1f0f42e30848d3fbc74135adcf91b5
dmkonq <149373616+dmkonq@users.noreply.github.com>
2023-11-16 12:51:20 +0900

    refactor: nuke codes for demonstration

--- main.go
+++ main.go
@@ -6,24 +6,5 @@ func foo() (string, error) {
 	return "", errors.New("not implemented")
 }
 
-func bar() (string, error) {
-	// asdfkjl
-	return "bar", nil
-}
-
-// asdf
-func baz() (string, error) {
-	return "baz", nil
-}
-
-func qux() (string, error) {
-	return "", errors.New("not implemented")
-}
-
-// yeet yeets
-func yeet() (string, error) {
-	return "YEET", nil
-}
-
 func main() {
 }
